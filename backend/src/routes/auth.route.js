cba599cfa16a52ccd9f315a53f09f67d5ecfccfe
Krishan Bandara <Krishan.Bandara@softlogiclife.lk>
2025-09-27 14:38:12 +0530

    ready for deployment

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,17 +2,16 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/signup",(req,res) =>{
-    res.send("Signup endpoint");
-})
+router.get("/signup", (req, res) => {
+  res.send("Signup endpoint");
+});
 
-router.get("/login",(req,res) =>{
-    res.send("Login endpoint");
-})
+router.get("/login", (req, res) => {
+  res.send("Login endpoint");
+});
 
-router.get("/logout",(req,res) =>{
-    res.send("logout endpoint");
-})
-
+router.get("/logout", (req, res) => {
+  res.send("logout endpoint");
+});
 
 export default router;
